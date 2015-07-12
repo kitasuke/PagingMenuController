1893adf9c65573433df9eb6474e664fe8b136da2
kitasuke <yusuke2759@gmail.com>
2015-07-12 16:55:42 +0900

    Add configuration files

---
+++ Example/Pods/Target Support Files/Pods-PagingMenuControllerDemo2-PagingMenuController/Pods-PagingMenuControllerDemo2-PagingMenuController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PagingMenuControllerDemo2_PagingMenuController : NSObject
+@end
+@implementation PodsDummy_Pods_PagingMenuControllerDemo2_PagingMenuController
+@end
