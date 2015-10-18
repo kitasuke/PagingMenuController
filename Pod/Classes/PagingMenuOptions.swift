11ec8511847d06b46ba3affbce7c4540b2a23fe0
kitasuke <yusuke2759@gmail.com>
2015-10-18 12:13:55 +0900

    Add decelerating rate option for menu view

--- Pod/Classes/PagingMenuOptions.swift
+++ Pod/Classes/PagingMenuOptions.swift
@@ -21,6 +21,7 @@ public class PagingMenuOptions {
     public var menuHeight: CGFloat = 50
     public var menuItemMargin: CGFloat = 20
     public var animationDuration: NSTimeInterval = 0.3
+    public var deceleratingRate: CGFloat = UIScrollViewDecelerationRateNormal
     public var menuDisplayMode = MenuDisplayMode.Standard(widthMode: PagingMenuOptions.MenuItemWidthMode.Flexible, centerItem: false, scrollingMode: PagingMenuOptions.MenuScrollingMode.PagingEnabled)
     public var menuItemMode = MenuItemMode.Underline(height: 3, color: UIColor.blueColor(), horizontalPadding: 0, verticalPadding: 0)
     internal var menuItemCount = 0
