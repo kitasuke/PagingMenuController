fb4d4181f6bd7912ec2b1b0323235cc32491002e
kitasuke <yusuke2759@gmail.com>
2016-06-09 19:04:26 +0900

    Update example project

--- Example/PagingMenuControllerDemo/PagingMenuViewController.swift
+++ Example/PagingMenuControllerDemo/PagingMenuViewController.swift
@@ -18,7 +18,6 @@ class PagingMenuViewController: UIViewController {
         
         let pagingMenuController = self.childViewControllers.first as! PagingMenuController
         pagingMenuController.delegate = self
-        pagingMenuController.menuView?.viewDelegate = self
         pagingMenuController.setup(options)
     }
 }
@@ -27,36 +26,14 @@ extension PagingMenuViewController: PagingMenuControllerDelegate {
     // MARK: - PagingMenuControllerDelegate
 
     func willMoveToPageMenuController(menuController: UIViewController, previousMenuController: UIViewController) {
-        print(#function)
-        print(previousMenuController)
-        print(menuController)
-        let pagingMenuController = self.childViewControllers.first as! PagingMenuController
-        print(pagingMenuController.currentPage)
     }
 
     func didMoveToPageMenuController(menuController: UIViewController, previousMenuController: UIViewController) {
-        print(#function)
-        print(previousMenuController)
-        print(menuController)
-        let pagingMenuController = self.childViewControllers.first as! PagingMenuController
-        print(pagingMenuController.currentPage)
     }
-}
-
-extension PagingMenuViewController: MenuViewDelegate {
+    
     func willMoveToMenuItemView(menuItemView: MenuItemView, previousMenuItemView: MenuItemView) {
-        print(#function)
-        print(previousMenuItemView)
-        print(menuItemView)
-        let pagingMenuController = self.childViewControllers.first as! PagingMenuController
-        print(pagingMenuController.currentPage)
     }
     
     func didMoveToMenuItemView(menuItemView: MenuItemView, previousMenuItemView: MenuItemView) {
-        print(#function)
-        print(previousMenuItemView)
-        print(menuItemView)
-        let pagingMenuController = self.childViewControllers.first as! PagingMenuController
-        print(pagingMenuController.currentPage)
     }
 }
