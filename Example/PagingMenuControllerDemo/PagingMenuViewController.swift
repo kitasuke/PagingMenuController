083102dab28c27dc1fc6d0f516203fc8f0aa2249
kitasuke <yusuke2759@gmail.com>
2016-06-09 15:45:39 +0900

    Update example project

--- Example/PagingMenuControllerDemo/PagingMenuViewController.swift
+++ Example/PagingMenuControllerDemo/PagingMenuViewController.swift
@@ -10,32 +10,16 @@ import UIKit
 import PagingMenuController
 
 class PagingMenuViewController: UIViewController {
-    var options = PagingMenuOptions()
+    var options: PagingMenuControllerCustomizable!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let usersViewController = UsersViewController.instantiateFromStoryboard()
-        usersViewController.menuItemDescription = menuItemDescription
-        let repositoriesViewController = RepositoriesViewController.instantiateFromStoryboard()
-        repositoriesViewController.menuItemDescription = menuItemDescription
-        let gistsViewController = GistsViewController.instantiateFromStoryboard()
-        gistsViewController.menuItemDescription = menuItemDescription
-        let organizationsViewController = OrganizationsViewController.instantiateFromStoryboard()
-        organizationsViewController.menuItemDescription = menuItemDescription
-        
-        let viewControllers = [usersViewController, repositoriesViewController, gistsViewController, organizationsViewController]
-        
         let pagingMenuController = self.childViewControllers.first as! PagingMenuController
         pagingMenuController.delegate = self
-        
-        switch options.menuComponentType {
-        case .All, .MenuController:
-            pagingMenuController.setup(viewControllers, options: options)
-        case .MenuView:
-            pagingMenuController.setup(viewControllers.map { $0.title! }, options: options)
-        }
+        pagingMenuController.menuView?.viewDelegate = self
+        pagingMenuController.setup(options)
     }
 }
 
@@ -43,10 +27,36 @@ extension PagingMenuViewController: PagingMenuControllerDelegate {
     // MARK: - PagingMenuControllerDelegate
 
     func willMoveToPageMenuController(menuController: UIViewController, previousMenuController: UIViewController) {
-
+        print(#function)
+        print(previousMenuController)
+        print(menuController)
+        let pagingMenuController = self.childViewControllers.first as! PagingMenuController
+        print(pagingMenuController.currentPage)
     }
 
     func didMoveToPageMenuController(menuController: UIViewController, previousMenuController: UIViewController) {
+        print(#function)
+        print(previousMenuController)
+        print(menuController)
+        let pagingMenuController = self.childViewControllers.first as! PagingMenuController
+        print(pagingMenuController.currentPage)
+    }
+}
 
+extension PagingMenuViewController: MenuViewDelegate {
+    func willMoveToMenuItemView(menuItemView: MenuItemView, previousMenuItemView: MenuItemView) {
+        print(#function)
+        print(previousMenuItemView)
+        print(menuItemView)
+        let pagingMenuController = self.childViewControllers.first as! PagingMenuController
+        print(pagingMenuController.currentPage)
+    }
+    
+    func didMoveToMenuItemView(menuItemView: MenuItemView, previousMenuItemView: MenuItemView) {
+        print(#function)
+        print(previousMenuItemView)
+        print(menuItemView)
+        let pagingMenuController = self.childViewControllers.first as! PagingMenuController
+        print(pagingMenuController.currentPage)
     }
 }
