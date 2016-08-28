3e7669106e0d4bec138f70fd0fa624a2dcb6d156
kitasuke <yusuke2759@gmail.com>
2016-08-28 17:06:53 +0900

    Remove optional protocol method

--- Pod/Classes/Protocols/GestureHandler.swift
+++ Pod/Classes/Protocols/GestureHandler.swift
@@ -8,28 +8,28 @@
 
 import Foundation
 
-@objc protocol GestureHandler {
+protocol GestureHandler {
     func addTapGestureHandler()
     func addSwipeGestureHandler()
-    @objc optional func handleTapGesture(_ recognizer: UITapGestureRecognizer)
-    @objc optional func handleSwipeGesture(_ recognizer: UISwipeGestureRecognizer)
+    func handleTapGesture(_ recognizer: UITapGestureRecognizer)
+    func handleSwipeGesture(_ recognizer: UISwipeGestureRecognizer)
 }
 
 extension GestureHandler {
     var tapGestureRecognizer: UITapGestureRecognizer {
-        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(PagingMenuController.handleTapGesture(_:)))
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(PagingMenuController.handleTapGesture))
         gestureRecognizer.numberOfTapsRequired = 1
         return gestureRecognizer
     }
     
     var leftSwipeGestureRecognizer: UISwipeGestureRecognizer {
-        let gestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(PagingMenuController.handleSwipeGesture(_:)))
+        let gestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(PagingMenuController.handleSwipeGesture))
         gestureRecognizer.direction = .left
         return gestureRecognizer
     }
     
     var rightSwipeGestureRecognizer: UISwipeGestureRecognizer {
-        let gestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(PagingMenuController.handleSwipeGesture(_:)))
+        let gestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(PagingMenuController.handleSwipeGesture))
         gestureRecognizer.direction = .right
         return gestureRecognizer
     }
